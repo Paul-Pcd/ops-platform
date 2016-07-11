952affdbd6e754170f39a0e70499ab64f8d2a990
荣俊峰 <rongjunfeng@ucfgroup.com>
2016-07-11 17:15:35 +0800

    资源平台首页使用前端框架展示

--- opsweb/dashboard/views.py
+++ opsweb/dashboard/views.py
@@ -1,7 +1,8 @@
+# coding:utf-8
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 
 def index_view(request):
-    return HttpResponse("<h1> resources index </h1>")
+    return render(request,"resources/index.html", {"title": "首页"})
