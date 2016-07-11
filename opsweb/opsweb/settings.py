afcb776f494c4ac46cd9fb1ae71e7d69c05016ec
荣俊峰 <rongjunfeng@ucfgroup.com>
2016-07-11 16:25:42 +0800

    增加开发服务器的配置文件

--- opsweb/opsweb/settings.py
+++ opsweb/opsweb/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'v$gox-p9%wn6*^xq1)(hip0(fpld@(wtg57cn_llxtra68bpky'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
